f1a5971874bfc9852d87ec75b99b1ddb6f53f16a
Radik-TRN <radik_trofimov@mail.ru>
2023-02-14 11:44:57 +0300

    Комплексные типы данных: списки

---
+++ task8.py
@@ -0,0 +1,18 @@
+phones = [3, 5, 7, 9, 10.5]
+print(phones)
+
+phones.append('Python')
+print(phones)
+
+print(len(phones))
+
+print(phones[0])
+
+print(phones[-1])
+
+print(phones[2:5])
+
+phones.remove('Python')
+print(phones)
+print(len(phones))
+
